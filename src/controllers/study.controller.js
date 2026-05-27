6caae7df2133826c87c6ab08aee1520cf5a67caf
choihoomba <talote33@naver.com>
2026-05-27 23:22:03 +0900

    feat: Study API 기능 추가

---
+++ src/controllers/study.controller.js
@@ -0,0 +1,146 @@
+import prisma from '../lib/prisma.js';
+import {
+  deleteStudySchema,
+  studyBodySchema,
+  studyParamsSchema,
+  studyQuerySchema,
+  updateStudySchema,
+} from '../schemas/study.schema.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import {
+  NotFoundError,
+  Unauthorized,
+  ValidationError,
+} from '../utils/errors.js';
+
+export const createStudy = asyncHandler(async (req, res) => {
+  const { nickname, title, description, background, password } =
+    studyBodySchema.parse(req.body);
+  const newStudy = await prisma.study.create({
+    data: {
+      nickname,
+      title,
+      description,
+      background,
+      password,
+    },
+  });
+  res
+    .status(201)
+    .json({ success: true, data: newStudy, message: '스터디 등록 성공' });
+});
+
+export const getStudies = asyncHandler(async (req, res) => {
+  const {
+    keyword,
+    cursor,
+    limit = 6,
+    sort,
+  } = studyQuerySchema.parse(req.query);
+  let orderBy;
+  switch (sort) {
+    case 'recent':
+      orderBy = { createdAt: 'desc' };
+      break;
+    case 'oldest':
+      orderBy = { createdAt: 'asc' };
+      break;
+    case 'pointDesc':
+      orderBy = { totalPoint: 'desc' };
+      break;
+    case 'pointAsc':
+      orderBy = { totalPoint: 'asc' };
+      break;
+    default:
+      orderBy = { createdAt: 'desc' };
+  }
+  const where = keyword
+    ? {
+        OR: [
+          { nickname: { contains: keyword } },
+          { title: { contains: keyword } },
+          { description: { contains: keyword } },
+        ],
+      }
+    : {};
+  const studies = await prisma.study.findMany({
+    where,
+    take: limit + 1,
+    skip: cursor ? 1 : 0,
+    cursor: cursor ? { id: cursor } : undefined,
+    orderBy,
+  });
+  const hasNext = studies.length > limit;
+  const data = studies.slice(0, limit);
+  const nextCursor = hasNext ? data[data.length - 1].id : null;
+  res.status(200).json({
+    success: true,
+    data,
+    nextCursor,
+  });
+});
+
+export const getStudy = asyncHandler(async (req, res) => {
+  const { id } = studyParamsSchema.parse(req.params);
+  const study = await prisma.study.findUnique({
+    where: { id },
+    include: {
+      habits: true,
+      emojis: true,
+    },
+  });
+  if (!study) {
+    throw new NotFoundError();
+  }
+  res.status(200).json({ success: true, data: study });
+});
+
+export const updateStudy = asyncHandler(async (req, res) => {
+  const { id } = studyParamsSchema.parse(req.params);
+  const {
+    nickname,
+    title,
+    description,
+    background,
+    password,
+    totalPoint,
+    newPassword,
+  } = updateStudySchema.parse(req.body);
+  const studyPasswordValidation = await prisma.study.findUnique({
+    where: { id },
+  });
+  if (!studyPasswordValidation) throw new NotFoundError();
+  if (studyPasswordValidation.password !== password) throw new Unauthorized();
+  if (newPassword === password)
+    throw new ValidationError('기존 비밀번호와 동일합니다');
+
+  const study = await prisma.study.update({
+    where: { id },
+    data: {
+      nickname,
+      title,
+      description,
+      background,
+      totalPoint,
+      password: newPassword,
+    },
+  });
+  res
+    .status(200)
+    .json({ success: true, data: study, message: '스터디 수정 성공' });
+});
+
+export const deleteStudy = asyncHandler(async (req, res) => {
+  const { id } = studyParamsSchema.parse(req.params);
+  const { password } = deleteStudySchema.parse(req.body);
+  const studyPasswordValidation = await prisma.study.findUnique({
+    where: { id },
+  });
+  if (!studyPasswordValidation) throw new NotFoundError();
+  if (studyPasswordValidation.password !== password) throw new Unauthorized();
+
+  await prisma.study.delete({
+    where: { id },
+  });
+  res.status(200).json({ success: true, message: '스터디 삭제 성공' });
+});
