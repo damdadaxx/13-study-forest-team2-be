da76d511057132433014103d189effb3aa441654
choihoomba <talote33@naver.com>
2026-06-03 00:02:48 +0900

    fix: getStudies 보조 정렬기준 추가

--- src/controllers/study.controller.js
+++ src/controllers/study.controller.js
@@ -40,19 +40,19 @@ export const getStudies = asyncHandler(async (req, res) => {
   let orderBy;
   switch (sort) {
     case 'recent':
-      orderBy = { createdAt: 'desc' };
+      orderBy = [{ createdAt: 'desc' }, { id: 'desc' }];
       break;
     case 'oldest':
-      orderBy = { createdAt: 'asc' };
+      orderBy = [{ createdAt: 'asc' }, { id: 'asc' }];
       break;
     case 'pointDesc':
-      orderBy = { totalPoint: 'desc' };
+      orderBy = [{ totalPoint: 'desc' }, { id: 'desc' }];
       break;
     case 'pointAsc':
-      orderBy = { totalPoint: 'asc' };
+      orderBy = [{ totalPoint: 'asc' }, { id: 'asc' }];
       break;
     default:
-      orderBy = { createdAt: 'desc' };
+      orderBy = [{ createdAt: 'desc' }, { id: 'desc' }];
   }
   const where = keyword
     ? {
