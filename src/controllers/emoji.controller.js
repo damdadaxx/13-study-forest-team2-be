b65b42cdd87c72cd19ef41f700ea19fd97e71028
damdadaxx <hyojudev@gmail.com>
2026-06-03 23:45:26 +0900

    feat: Emoji 증감 API 추가
    - decrementEmoji 추가 (count 감소 및 필요 시 삭제)
    - upsertEmoji > incrementEmoji로 리네이밍
    - 이모지 추가 API를 POST 메서드로 변경
    - upsertEmojiSchema > emojiBodySchema로 리네이밍

--- src/controllers/emoji.controller.js
+++ src/controllers/emoji.controller.js
@@ -1,10 +1,7 @@
 import prisma from '../lib/prisma.js';
-import {
-  emojiParamsSchema,
-  upsertEmojiSchema,
-} from '../schemas/emoji.schema.js';
+import { emojiParamsSchema, emojiBodySchema } from '../schemas/emoji.schema.js';
 import asyncHandler from '../utils/asyncHandler.js';
-import { NotFoundError, ValidationError } from '../utils/errors.js';
+import { NotFoundError } from '../utils/errors.js';
 
 // GET /studies/:studyId/emojis
 export const getAllEmojis = asyncHandler(async (req, res) => {
@@ -27,12 +24,12 @@ export const getAllEmojis = asyncHandler(async (req, res) => {
   });
 });
 
-// PATCH /studies/:studyId/emojis
-export const upsertEmoji = asyncHandler(async (req, res) => {
+// POST /studies/:studyId/emojis
+export const incrementEmoji = asyncHandler(async (req, res) => {
   const { studyId } = emojiParamsSchema.parse(req.params);
-  const data = upsertEmojiSchema.parse(req.body);
-  const { emoji } = data;
+  const { emoji } = emojiBodySchema.parse(req.body);
 
+  // 이모지 카운트 업 후 필요 시 생성
   const newEmoji = await prisma.emoji.upsert({
     where: {
       // unique constraint
@@ -53,4 +50,69 @@ export const upsertEmoji = asyncHandler(async (req, res) => {
   const statusCode = isCreated ? 201 : 200;
 
   res.status(statusCode).json({ success: true, data: newEmoji });
+});
+
+// DELETE /studies/:studyId/emojis
+export const decrementEmoji = asyncHandler(async (req, res) => {
+  const { studyId } = emojiParamsSchema.parse(req.params);
+  const { emoji } = emojiBodySchema.parse(req.body);
+
+  // 이모지 카운트 감소 후 필요 시 삭제
+  const result = await prisma.$transaction(async (tx) => {
+    const targetEmoji = await tx.emoji.findUnique({
+      where: {
+        studyId_emoji: {
+          studyId,
+          emoji,
+        },
+      },
+    });
+
+    if (!targetEmoji) {
+      throw new NotFoundError('해당 이모지를 찾을 수 없습니다.');
+    }
+
+    // 이모지 count가 1이하일 때, 이모지 삭제
+    if (targetEmoji.count <= 1) {
+      await tx.emoji.delete({
+        where: {
+          studyId_emoji: {
+            studyId,
+            emoji,
+          },
+        },
+      });
+
+      return {
+        deleted: true,
+        message: '이모지가 삭제되었습니다.',
+      };
+    }
+
+    // 이모지 카운트 다운
+    const updatedEmoji = await tx.emoji.update({
+      where: {
+        studyId_emoji: {
+          studyId,
+          emoji,
+        },
+      },
+      data: {
+        count: {
+          decrement: 1,
+        },
+      },
+    });
+
+    return {
+      deleted: false,
+      message: '이모지 카운트가 감소했습니다.',
+      data: updatedEmoji,
+    };
+  });
+
+  return res.json({
+    success: true,
+    ...result,
+  });
 });
--- src/routes/emoji.routes.js
+++ src/routes/emoji.routes.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { getAllEmojis, upsertEmoji } from '../controllers/emoji.controller.js';
+import {
+  getAllEmojis,
+  incrementEmoji,
+  decrementEmoji,
+} from '../controllers/emoji.controller.js';
 
 const router = express.Router({ mergeParams: true });
 
 // GET /studies/:studyId/emojis - 이모지 목록 조회
 router.get('/', getAllEmojis);
-// PATCH /studies/:studyId/emojis - 이모지 생성 / 카운트 업
-router.patch('/', upsertEmoji);
+// POST /studies/:studyId/emojis - 이모지 추가 및 count 증가
+router.post('/', incrementEmoji);
+// DELETE /studies/:studyId/emojis - 이모지 제거 및 count 감소
+router.delete('/', decrementEmoji);
 
 export default router;
